3545448b3d51d1964fa58c93e5aa55635ace723d
Mustapha Ibrahim <ibrahimmustapha12345678@gmail.com>
2024-12-24 09:41:09 +0100

    ok

--- src/info.jsx
+++ src/info.jsx
@@ -3,7 +3,27 @@ import React from 'react';
 function Info(){
     return(
         <div className='container2'>
-            <p>hi</p>
+            <div className='container21'>
+                <h1>Personal info</h1>
+                <p className='please' >Please provide your name, email address, and phone number.</p>
+            </div>
+            <div className='container22'>
+                <div className='bodies'>
+                    <p className='stands'>Name</p>
+                    <input className="inputer" placeholder="    e.g. Stephen King"></input>
+                </div>
+                <div className='bodies'>
+                    <p className='stands'>Email Address</p>
+                    <input className="inputer" placeholder="    e.g. stephenking@lorem.com"></input>
+                </div>
+                <div className='bodies'>
+                    <p className='stands'>Phone Number</p>
+                    <input className="inputer" placeholder="    e.g. +1 234 567 890"></input>
+                </div>
+            </div>
+            <div className='container23'>
+                <button className='button'>Next Step</button>
+            </div>
         </div>
     )
 }
--- src/side.jsx
+++ src/side.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import sidebar from "./images/bg-sidebar-desktop.svg";
 function Side(){
     return(
         <div className='container1'>
-            <div>
-                
+            <div className='container11'>
+                <img className='sidebar' src={sidebar}/>
+                <div className='numbers'>
+                    <p>1</p>
+                    <p>2</p>
+                    <p>3</p>
+                    <p>4</p>
+                </div>
+                <div>
+
+                </div>
             </div>
         </div>
     )
